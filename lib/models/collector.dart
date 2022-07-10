6e24c6e0e2b77f0d12d69eabcdd3ed9bed0d16ad
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-07-10 21:49:35 +0800

    feat(collector): add collection properties

--- lib/models/collector.dart
+++ lib/models/collector.dart
@@ -1,9 +1,18 @@
+import 'package:collection_report/models/collection.dart';
+
 class Collector {
   String id;
   late String name;
+  late Collection monthly;
+  Collection? daily;
 
   Collector(this.id, Map<String, dynamic> data) {
     name = data['name'];
+    monthly = Collection.fromJson('$name (Monthly)', data['monthly']);
+
+    if (data['daily'] != null) {
+      daily = Collection.fromJson('$name (Daily)', data['daily']);
+    }
   }
 
   Map<String, dynamic> info() {
