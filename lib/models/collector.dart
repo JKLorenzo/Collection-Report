6ec2297027ddaa7daf4e17150c2acc69859b50a9
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-07-12 03:23:55 +0800

    feat(collector): add card view

--- lib/models/collector.dart
+++ lib/models/collector.dart
@@ -1,4 +1,5 @@
 import 'package:collection_report/models/collection.dart';
+import 'package:flutter/material.dart';
 
 class Collector {
   String id;
@@ -25,5 +26,66 @@ class Collector {
       'monthly': Collection.template().toJson(),
       'daily': Collection.template().toJson()
     };
+  }
+
+  Widget cardView() {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: [
+          const Icon(Icons.person_rounded, size: 90),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            const Text(
+                              'Monthly',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 5),
+                            Text('${monthly.total}'),
+                          ],
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          children: [
+                            const Text(
+                              'Daily',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 5),
+                            Text('${daily.total}'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
